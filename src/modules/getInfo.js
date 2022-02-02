580a283b2ee250b42d054ddeca53a433abbbbf6f
uae <aniekaudo@yahoo.com>
2022-02-02 12:59:40 +0100

    Added API data to popup menu

--- src/modules/getInfo.js
+++ src/modules/getInfo.js
@@ -1,6 +1,6 @@
 const getInfo = async () => {
   const getMeals = await fetch(
-    'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772'
+    'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772',
   );
   return (await getMeals.json()).meals;
 };
