0bae6f5b43707729601a4711781e733577bb4096
uae <aniekaudo@yahoo.com>
2022-02-02 22:25:12 +0100

    Fixing linters

--- src/modules/displayItems.js
+++ src/modules/displayItems.js
@@ -77,7 +77,7 @@ import { addLikeToAPI, getLikesFromAPI } from './likes.js';
 // import homepageCounter from './homepageCounter.js';
 
 const mealsContainer = document.querySelector('.cards-wrapper');
-const numbersOfMeals = document.querySelector('.meals-number');
+// const numbersOfMeals = document.querySelector('.meals-number');
 
 const list = (meals) => {
   meals.forEach(async (meal) => {
