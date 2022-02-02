a69e998c56ae51ca53e83eb733f090e3342a1670
uae <aniekaudo@yahoo.com>
2022-02-02 19:47:20 +0100

    Modified pop-up menu to display single pop-up item

--- src/modules/getInfo.js
+++ src/modules/getInfo.js
@@ -1,6 +1,6 @@
 const getInfo = async (idMeal) => {
   const getMealsInfo = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`,
   );
   return (await getMealsInfo.json()).meals;
 };
